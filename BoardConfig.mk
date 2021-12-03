b7e322db56b26f0d62baa75a05266b33f477e03c
PixelBoot <bootingpixels@gmail.com>
2021-12-03 21:43:21 +0100

    Revert "[DNM] wahoo: build permissive kernel"
    This reverts commit cd37d4a77f97d9f1ce5e507c5230b4d7cf2e3525.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,6 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME) androidboot.console=ttyMSM0 lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
